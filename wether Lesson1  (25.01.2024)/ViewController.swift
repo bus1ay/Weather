cf502ac4c9ac993a9b0be68b1d524a870e2ed150
bus1ay <buslaevigor2003@gmail.com>
2024-01-25 22:26:32 +0300

    First Commit
    First Commit

---
+++ wether Lesson1  (25.01.2024)/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  wether Lesson1  (25.01.2024)
+//
+//  Created by Игорь Буслаев on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, Igor")
+        
+    }
+
+
+}
+
