f9847bf3f9f56bde12e1d7f1274074d3323915b9
bus1ay <buslaevigor2003@gmail.com>
2024-01-26 00:54:48 +0300

    Two commit
    arrange stotyBoard

--- wether Lesson1  (25.01.2024)/ViewController.swift
+++ wether Lesson1  (25.01.2024)/ViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var tempetureLabel: UILabel!
+    
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    
+    @IBAction func refreshButtonTaped(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello, Igor")
+       
         
     }
 
